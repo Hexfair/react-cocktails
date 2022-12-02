8b6f9fa059571ce23327f240b885e8d8d996723d
Hexfair <Hexfair17@gmail.com>
2022-12-02 08:07:38 -0500

    Footer and Options

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from './Footer.module.scss';
 
 export const Footer = () => {
 	return (
-		<div>Footer</div>
+		<div className={styles.footer}>
+			<div className={styles.create}>Created by <span>Hexfair</span></div>
+			<div className={styles.back}>Home</div>
+		</div>
 	)
 }
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Options.module.scss';
+
+export const Options = () => {
+	return (
+		<div className={styles.options}>Options</div>
+	)
+}
+
