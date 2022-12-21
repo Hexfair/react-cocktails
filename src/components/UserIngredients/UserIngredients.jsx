b0b2fa151c9d9c555947ac42b7a6a161b073e2e2
Hexfair <hexfair17@gmail.com>
2022-12-20 20:22:09 -0500

    IngredientImage

--- src/components/UserIngredients/UserIngredients.jsx
+++ src/components/UserIngredients/UserIngredients.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import styles from './UserIngredients.module.scss';
 import { useSelector } from "react-redux";
 import { selectUserIngredients } from "../../redux/userCocktails/userCocktails-selectors";
-import { getSmallImageOfIngredient } from '../../api/api'
+import { IngredientImage } from "../IngredientImage/IngredientImage";
+// import { getSmallImageOfIngredient } from '../../api/api';
+// import noImage from '../../assets/no-image.png'
 //=========================================================================================================================
 
 export const UserIngredients = ({ idx }) => {
@@ -12,7 +14,8 @@ export const UserIngredients = ({ idx }) => {
 		<div className={styles.ingredients}>
 			{ingredientsArray && ingredientsArray.map((obj, index) =>
 				<div className={styles.ingredient} key={index} >
-					<img src={getSmallImageOfIngredient(obj)} alt='' />
+					<IngredientImage index={index} obj={obj} />
+					{/* <img src={vis ? getSmallImageOfIngredient(obj) : noImage} alt='' onLoad={() => lll(index)} onError={() => eee(index)} /> */}
 					<span className={styles.caption} >{obj}:</span>
 					<span className={styles.dose}>{measuresArray[index] || 'taste'}</span>
 				</div>
@@ -20,3 +23,4 @@ export const UserIngredients = ({ idx }) => {
 		</div>
 	)
 }
+
--- src/redux/userCocktails/userCocktails-selectors.js
+++ src/redux/userCocktails/userCocktails-selectors.js
@@ -1,7 +1,5 @@
 export const selectUserIngredients = (index) => (state) => {
-	console.log(index);
 	const item = state.userCocktails.userCocktailsList[index];
-	console.log('item', item);
 	const entries = item ? Object.entries(item) : [];
 	const ingredientsArray = [];
 	const measuresArray = [];
